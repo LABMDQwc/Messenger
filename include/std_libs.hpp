3a4838815240a00794052282f8fb85fab7eeea16
LABMDQwc <hqlvtetrajois@gmail.com>
2023-07-24 15:36:13 +0300

    add include folder

---
+++ include/std_libs.hpp
@@ -0,0 +1,6 @@
+#ifndef _MESSENGER_STD_LIBS_HPP
+#define _MESSENGER_STD_LIBS_HPP
+
+#include <iostream>
+
+#endif  //_MESSENGER_STD_LIBS_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+#include "../include/std_libs.hpp"
+
 int main() {
-	return 0;
+  std::string ip;
+  getline(std::cin, ip);
+  return 0;
 }
